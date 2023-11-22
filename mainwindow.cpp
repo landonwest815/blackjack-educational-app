89553ee703149cd5b6f2e3ce194893a8bfe83a6b
Landon West <landonwest815@gmail.com>
2023-11-22 16:01:01 -0700

    Setup some basic UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QImage image(":/cards/5H.png");
+    QPixmap pix;
+    pix.convertFromImage(image);
+
+    ui->dealerCards->setPixmap(pix);
+
+
+    QImage image2(":/cards/QS.png");
+    QPixmap pix2;
+    pix2.convertFromImage(image2);
+
+    ui->playerCards->setPixmap(pix2);
+
 }
 
 MainWindow::~MainWindow()
